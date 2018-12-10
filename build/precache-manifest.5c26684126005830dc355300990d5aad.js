759594fa4ef62caf0d587cafff5938bca8ff3af9
ToToRo Yoo <woshiydh08@gmail.com>
2018-12-10 09:59:59 +0800

    day1 commit 1 project start

---
+++ build/precache-manifest.5c26684126005830dc355300990d5aad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c1a2a02e3de1d96376c2",
+    "url": "/static/js/main.c1a2a02e.chunk.js"
+  },
+  {
+    "revision": "d0353bf1afe4f4d7ba0e",
+    "url": "/static/js/1.d0353bf1.chunk.js"
+  },
+  {
+    "revision": "c1a2a02e3de1d96376c2",
+    "url": "/static/css/main.a6ebba48.chunk.css"
+  },
+  {
+    "revision": "f2d04088a43d2b55652d073da3f86d50",
+    "url": "/index.html"
+  }
+];
